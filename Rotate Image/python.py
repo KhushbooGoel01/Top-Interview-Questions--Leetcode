55741cfb0a69a4bdb85a59a6de026d25e2415e49
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-08-11 11:40:20 +0530

    Update python.py

--- Rotate Image/python.py
+++ Rotate Image/python.py
@@ -2,7 +2,10 @@ video : https://www.youtube.com/watch?v=uSanzwcYEZg&list=PLlMTxcUZWpmBjE3Zm8pdgG
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
+        
         for i in range(len(matrix)):
-            for j in range(i):
-                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j] #this is to transpose matrix
-        matrix.reverse()
+            for j in range(i,len(matrix)):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j] 
+                #this is to transpose matrix
+        for l in matrix:
+            l.reverse()
