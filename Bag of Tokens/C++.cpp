a36ad8d9e6554a45256908a44fba727173275725
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-08-11 16:44:55 +0530

    Update and rename C++ to C++.cpp

---
+++ Bag of Tokens/C++.cpp
@@ -0,0 +1,25 @@
+video Link:
+
+ int bagOfTokensScore(vector<int>& tokens, int P) {
+        sort(tokens.begin(), tokens.end());
+        
+        int res = 0, points = 0, i = 0, j = tokens.size() - 1;
+        
+        while (i <= j) {
+            if (P >= tokens[i]) {
+                P -= tokens[i];
+                i++;
+                res = max(res, ++points);
+                
+            }
+            else if (points > 0) {
+                points--;
+                P += tokens[j];
+                j--;
+            }
+            else {
+                break;
+            }
+        }
+        return res;
+    }
