d2460ddcf28bf434656365f097e3bfce0bd3b0bb
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-08-11 16:42:30 +0530

    Create python.py

---
+++ Bag of Tokens/python.py
@@ -0,0 +1,16 @@
+Video link:
+
+def bagOfTokensScore(self, tokens, P):
+        res = cur = 0
+        d = collections.deque(sorted(tokens))
+        
+        while d and (d[0] <= P or cur):
+            if d[0] <= P:
+                P -= d.popleft()
+                cur += 1
+            else:
+                P += d.pop()
+                cur -= 1
+            res = max(res, cur)
+            
+        return res
