e0c6b1d97b4b51f8f39b6982cc9842ad90261db0
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-08-13 15:47:04 +0530

    Update C++.cpp

--- Largest Rectangle in Histogram/C++.cpp
+++ Largest Rectangle in Histogram/C++.cpp
@@ -1,1 +1,20 @@
 Video Link:
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        int n = height.size(), area = 0, h, l;
+		stack<int> indexes;
+		for (int i = 0; i <= n; i++) {
+			while (i == n || (!indexes.empty() && height[indexes.top()] > height[i])) {
+				if (i == n && indexes.empty()) h = 0, i++;
+				else h = height[indexes.top()], indexes.pop();			
+				l = indexes.empty() ? -1 : indexes.top();
+				area = max(area, h * (i - l - 1));
+			}
+			indexes.push(i);
+		}
+		return area;
+	
+    }
+};
