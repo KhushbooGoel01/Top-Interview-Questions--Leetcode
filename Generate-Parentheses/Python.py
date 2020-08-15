516abcef43e603a45285d0201d50b8ade3a4e56f
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-08-15 16:19:42 +0530

    Create Python.py

---
+++ Generate-Parentheses/Python.py
@@ -0,0 +1,17 @@
+
+
+def generateParenthesis(self, n):
+    if n == 0:
+        return [""]
+    self.result = []
+    self.helper("", n, 0)
+    return self.result
+    
+def helper(self, pattern, remain_left, remain_right):
+    if remain_left == 0 and remain_right == 0:
+        self.result.append(pattern)
+        return
+    if remain_left:
+        self.helper(pattern + "(", remain_left - 1, remain_right + 1)
+    if remain_right:
+        self.helper(pattern + ")", remain_left, remain_right - 1)
