464746ef62feddc90ad0b926e01413018e8e0d86
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-08-15 16:17:22 +0530

    Create C++.cpp

---
+++ Generate-Parentheses/C++.cpp
@@ -0,0 +1,18 @@
+
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        addingpar(res, "", n, 0);
+        return res;
+    }
+    void addingpar(vector<string> &v, string str, int n, int m){
+        if(n==0 && m==0) {
+            v.push_back(str);
+            return;
+        }
+        if(m > 0){ addingpar(v, str+")", n, m-1); }
+        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+    }
+};
