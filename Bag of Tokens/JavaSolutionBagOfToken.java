34065cbdbf1d806367f2e2167432f02869fb5493
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-10-08 21:00:22 +0530

    Created Java Solution for Bag Of Token

---
+++ Bag of Tokens/JavaSolutionBagOfToken.java
@@ -0,0 +1,24 @@
+// Question link-https://leetcode.com/problems/bag-of-tokens/ 
+
+
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+
+        Arrays.sort(tokens);
+        if (tokens.length == 0 || power < tokens[0]) return 0;
+
+        int score = 0;
+        int i = 0, j = tokens.length -1;
+        while ( i <= j ) {
+            if (tokens[i] <= power) {
+                power -= tokens[i++];
+                score++;
+            } else if (i != j) {
+                power += tokens[j--];
+                score--; 
+            } else i++;
+        }
+        return score;
+
+    }
+}
