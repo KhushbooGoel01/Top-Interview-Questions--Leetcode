879fca320214f94e666a943205d60b0525a6bc31
Nayan Vishwakarma <52904138+nayan1xyz@users.noreply.github.com>
2021-10-10 20:43:05 +0530

    Update and rename Java.java to JavaSolutionGenerateParentheses.java
    Update and rename Java.java to JavaSolutionGenerateParentheses.java

---
+++ Generate-Parentheses/JavaSolutionGenerateParentheses.java
@@ -0,0 +1,16 @@
+//Question link: https://leetcode.com/problems/generate-parentheses/
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesis(c))
+                    for (String right: generateParenthesis(n-1-c))
+                        ans.add("(" + left + ")" + right);
+        }
+        return ans;
+    }
+}
