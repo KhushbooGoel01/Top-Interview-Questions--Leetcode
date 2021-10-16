51a3b2716a3bafed32165544f1ad04a28d560a9e
KhushbooGoel01 <aditikh2010@gmail.com>
2021-10-16 11:44:17 +0530

    update solutionOfRemoveNthNodeFromEndOfList

--- Remove Nth Node From End of List/Remove Nth Node From End of List.cpp
+++ Remove Nth Node From End of List/Remove Nth Node From End of List.cpp
@@ -1,39 +1,26 @@
 //Question Link:https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
-
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head,  int n) {
-        ListNode*p=head,*q=head;
-        if(head==NULL)
-            return NULL;
-        for(int i=0;i<n;i++)
-        {
-            if(p->next==NULL)
-            {
-                head=head->next;
-                return head;
-            }
-            p=p->next;
-        }
-        while(p->next!=NULL)
-        {
-            p=p->next;
-            q=q->next;
-        }
-        q->next=q->next->next;
-        return head;
-        
-    }
-};
+ class Solution {
+ public:
+     ListNode* removeNthFromEnd(ListNode* head, int n) {
+         if(!head||head->next==NULL&&n==1) return NULL;
+         ListNode *slow,*fast;
+         slow=head;
+         int length=0;
+         while(slow){
+             length++;
+             slow=slow->next;
+         }
+         length=length-n;
+         if(length){
+             slow=head;
+             length--;
+             while(length--){
+                 slow=slow->next;
+             }
+             slow->next=slow->next->next;
+             return head;
+         }
+         return head->next;
+     }
+ };
