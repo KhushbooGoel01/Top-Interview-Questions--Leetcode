97a9f5365b167e1b70b34a210e0725abdc256afd
Bhawna Chauhan <bhawnabc2002@gmail.com>
2021-10-21 16:13:08 +0530

    Add to merge

--- Missing Number/JavaSolutionMissingNumber.java
+++ Missing Number/JavaSolutionMissingNumber.java
@@ -1,31 +1,15 @@
 // Question Link :- https://leetcode.com/problems/missing-number/
 
 // Below is Solution
+
+//here i try to do the question in complexity O(n) and O(1) space
 class Solution {
     public int missingNumber(int[] nums) {
-        int i = 0;
-        while(i < nums.length){
-            int correct = nums[i];
-            if(nums[i] != nums.length && nums[i] != nums[correct]){
-                swap(nums, i, correct);
-            }else{
-                i++;
-            }
+        int sum = (nums.length * (nums.length + 1)) / 2;
+        for(int n : nums){
+            sum -= n;
         }
-        // search for first missing number
-        for(int index = 0; index < nums.length; index++){
-            if(nums[index] != index){
-                return index;
-            }
-        }
-        // case 2
-        // return number
-        return nums.length;
-    }
-    // swap
-    void swap(int[] arr, int first, int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        
+        return sum;
     }
 }
