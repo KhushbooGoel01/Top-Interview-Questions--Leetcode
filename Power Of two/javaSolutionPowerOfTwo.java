222170bc0338753816ee0c11bbfcc87d3c82c79b
omprakash72810 <91782649+omprakash72810@users.noreply.github.com>
2021-10-31 00:42:34 +0530

    Create javaSolutionPowerOfTwo.java

---
+++ Power Of two/javaSolutionPowerOfTwo.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%2==1) return false;
+        if(n==2)  return true;
+          
+        return isPowerOfTwo(n/2);
+    }
+}
